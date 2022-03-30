8513c662a76d4a09b37114bc5b49cfb685f4788c
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-30 01:10:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
 import About from './components/About';
-import Contact from './components/Contact';
 import Cover from './components/Cover';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Resume from './components/Resume';
 import Projects from './components/Projects';
 
 function App() {
@@ -16,8 +14,6 @@ function App() {
           <Cover />
           <About />
           <Projects />
-          <Contact />
-          <Resume />
         </>
       <Footer />
     </div>
