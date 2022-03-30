6ff0b1cc7f64b28e6690413e080855e9ad1e284c
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-30 01:41:00 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section class="hero is-medium is-link">
+    <section class="hero is-medium is-danger">
         <div class="hero-body">
             <p class="title">
                 About Me
