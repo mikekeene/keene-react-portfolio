a00b150a9202feed1b2b71803d68b2791cd0826c
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-30 01:10:38 -0500

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,15 +2,11 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <section className='resume-section'>
-			<div className='resume-container'>
-				<h1>Resume</h1>
-				<a href='https://drive.google.com/file/d/1B8YI3F1gMNZzLISUn2yc9iIhiOo2NGws/view?usp=sharing'>
-					Download my Resume
-				</a>
-				<br/>
-			</div>
-		</section>
+    <div id="resume" className="content has-text-centered">
+            <p>
+            <strong>Resume: </strong> <a href='https://drive.google.com/file/d/1B8YI3F1gMNZzLISUn2yc9iIhiOo2NGws/view?usp=sharing'>Michael Keene</a>
+            </p>
+    </div>
   )
 }
 
