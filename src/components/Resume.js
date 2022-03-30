caee359716c96d82f41b5ca35cfbe9c16486c1d5
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-30 17:32:21 -0500

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Resume = () => {
   return (
-    <div id="resume" className="content">
-            <p>
-            <strong>Resume: </strong> <a href='https://drive.google.com/file/d/1B8YI3F1gMNZzLISUn2yc9iIhiOo2NGws/view?usp=sharing'>Michael Keene</a>
-            </p>
-    </div>
+    <section id="resume" class="hero is-small is-danger">
+      <div class="hero-body">
+        <p class="title">
+          Resume
+        </p>
+        <a class="subtitle" href='https://drive.google.com/file/d/1B8YI3F1gMNZzLISUn2yc9iIhiOo2NGws/view?usp=sharing'>
+          <FontAwesomeIcon icon={faLink} className="mr-2 ml-2"/>
+          View
+        </a>
+      </div>
+    </section>
+    
+            
+    
   )
 }
 
