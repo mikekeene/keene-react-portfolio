f6625c28bcb046487643f5aa6681a1c2d3fbd1a8
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-29 22:05:12 -0500

    Update Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section class="hero is-info">
+        <div class="hero-body">
+            <p class="title">
+                Michael the Scream Queen Keene
+            </p>
+        <p class="subtitle">
+            High School Dropout, NO GED 
+        </p>
+    </div>
+    </section>
+
+  )
+}
+
+export default Header;
