cfde8a62c8f6713503522daf1dc4d11e9db4e9a9
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-29 21:54:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
-import About from './components/About';
+import { About } from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header';
