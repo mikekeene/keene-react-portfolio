be56e937c951b4af23c2bb7b07dbd026c1c23f34
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-30 01:10:29 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div> About me: I'm learning </div>
+    <section class="hero is-medium is-link">
+        <div class="hero-body">
+            <p class="title">
+                About Me
+            </p>
+            <p class="subtitle">
+                <h4 className='section-text'>
+                    My name is Michael Keene and I'm from San Antonio, TX.
+                    <br />
+                    I'm a H-E-B partner in warehousing and love it. 
+                    I'm also currently a student at UTSA's coding bootcamp.
+                    <br />
+                    I have a lot of interests but but my passions are spending time with 
+                    my family, reading or watching a good movie, and I enjoy playing video games
+
+                </h4>
+            </p>
+        </div>
+    </section>
   )
 }
 
