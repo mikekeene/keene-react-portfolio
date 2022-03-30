5666dfe67ee2977d237b609f0d88f46e08896fdf
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-29 22:51:02 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,73 @@
 import React from 'react'
-
+import budgetapp from '../assets/budget-tracker.png';
 const Projects = () => {
   return (
-    <div>Cover</div>
+    <div id="projects">
+        <div class="tile is-ancestor">
+
+            {/* budget tracker app */}
+            <div class="tile is-parent">
+                <article class="tile is-child box">
+                <p class="title">Budget Tracker</p>
+                <p class="subtitle">React</p>
+                <figure class="image is-4by3">
+                    <img src={budgetapp} alt={"budgetapp"} />
+                </figure>
+                </article>
+            </div>
+            {/* budget tracker app */}
+            <div class="tile is-parent">
+                <article class="tile is-child box">
+                <p class="title">Budget Tracker</p>
+                <p class="subtitle">React</p>
+                <figure class="image is-4by3">
+                    <img src={budgetapp} alt={"budgetapp"} />
+                </figure>
+                </article>
+            </div>
+            {/* budget tracker app */}
+            <div class="tile is-parent">
+                <article class="tile is-child box">
+                <p class="title">Budget Tracker</p>
+                <p class="subtitle">React</p>
+                <figure class="image is-4by3">
+                    <img src={budgetapp} alt={"budgetapp"} />
+                </figure>
+                </article>
+            </div>
+            {/* budget tracker app */}
+            <div class="tile is-parent">
+                <article class="tile is-child box">
+                <p class="title">Budget Tracker</p>
+                <p class="subtitle">React</p>
+                <figure class="image is-4by3">
+                    <img src={budgetapp} alt={"budgetapp"} />
+                </figure>
+                </article>
+            </div>
+            {/* budget tracker app */}
+            <div class="tile is-parent">
+                <article class="tile is-child box">
+                <p class="title">Budget Tracker</p>
+                <p class="subtitle">React</p>
+                <figure class="image is-4by3">
+                    <img src={budgetapp} alt={"budgetapp"} />
+                </figure>
+                </article>
+            </div>
+            {/* budget tracker app */}
+            <div class="tile is-parent">
+                <article class="tile is-child box">
+                <p class="title">Budget Tracker</p>
+                <p class="subtitle">React</p>
+                <figure class="image is-4by3">
+                    <img src={budgetapp} alt={"budgetapp"} />
+                </figure>
+                </article>
+            </div>
+        
+        </div>
+    </div>
   )
 }
 
