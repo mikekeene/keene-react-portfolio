5a64e3a172b6cf9b4b29ce1f2012d205fff58117
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-30 00:38:07 -0500

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,12 +5,9 @@ const Resume = () => {
     <section className='resume-section'>
 			<div className='resume-container'>
 				<h1>Resume</h1>
-				<p>
-					Download my<span> </span>
-						href='https://drive.google.com/file/d/1B8YI3F1gMNZzLISUn2yc9iIhiOo2NGws/view?usp=sharing'
-						target='_blank'
-						resume
-				</p>
+				<a href='https://drive.google.com/file/d/1B8YI3F1gMNZzLISUn2yc9iIhiOo2NGws/view?usp=sharing'>
+					Download my Resume
+				</a>
 				<br/>
 			</div>
 		</section>
