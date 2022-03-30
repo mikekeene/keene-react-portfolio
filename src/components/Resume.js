aba3d96b14040f40a2439fb3574fe2214bea7cb0
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-30 01:46:29 -0500

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div id="resume" className="content has-text-centered">
+    <div id="resume" className="content">
             <p>
             <strong>Resume: </strong> <a href='https://drive.google.com/file/d/1B8YI3F1gMNZzLISUn2yc9iIhiOo2NGws/view?usp=sharing'>Michael Keene</a>
             </p>
